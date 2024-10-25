6a04edeb47763e569611fc47d5d5401cf723d9e9
Deepa Khanal <115965444+khanal-deepa@users.noreply.github.com>
2024-10-25 17:40:18 +0545

    Update nltk_data_download.py

--- nltk_data_download.py
+++ nltk_data_download.py
@@ -1,13 +1,2 @@
 import nltk
-import os
-
-# Set the NLTK data path to the current working directory
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-nltk.data.path.append(nltk_data_path)
-
-# Download the required NLTK resources
-nltk.download('punkt', download_dir=nltk_data_path)
-nltk.download('stopwords', download_dir=nltk_data_path)
-nltk.download('wordnet', download_dir=nltk_data_path)
-
-print("Downloaded NLTK data in the current working environment.")
+nltk.download()
